df42a2443a9b3120187c8c8fe76e496681aef10d
lace04 <luisace04@gmail.com>
2023-08-04 20:31:45 -0500

    Challenge #23

---
+++ time-tracking-dashboard #23/main.js
@@ -0,0 +1,72 @@
+import data from './data.json' assert{type: 'json'}
+
+let bgColors =[
+    'hsl(15, 100%, 70%)',
+    'hsl(195, 74%, 62%)',
+    'hsl(348, 100%, 68%)',
+    'hsl(145, 58%, 55%)',
+    'hsl(264, 64%, 52%)',
+    'hsl(43, 84%, 65%)',
+]
+
+let dailyArray = data.map(item => item.timeframes.daily)
+let weeklyArray = data.map(item => item.timeframes.weekly)
+let monthlyArray = data.map(item => item.timeframes.monthly)
+
+console.log(dailyArray)
+console.log(weeklyArray)
+console.log(monthlyArray)
+
+
+let dailyBtn = document.querySelector('#daily');
+let weeklyBtn = document.querySelector('#weekly');
+let monthlyBtn = document.querySelector('#monthly');
+
+let secondsection = document.querySelector('.second-section');
+
+drawElements(dailyArray)
+
+dailyBtn.addEventListener('click', () => {
+    // console.log('daily button clicked');
+    drawElements(dailyArray)
+});
+
+weeklyBtn.addEventListener('click', () => {
+    drawElements(weeklyArray)
+});
+
+monthlyBtn.addEventListener('click', () => {
+    drawElements(monthlyArray)
+});
+
+function drawElements(array) {
+    secondsection.innerHTML = '';
+    array.forEach((element, index )=> {
+        // console.log(element)
+
+        let title = data[index].title;
+        let titlelowerCase = title.toLowerCase();
+
+        if(titlelowerCase == 'self care'){
+            titlelowerCase = 'self-care'
+        }
+
+        secondsection.innerHTML += `
+        <div class="card">
+            <div class="card__background" style="background-color: ${bgColors[index]};">
+            <img src="./images/icon-${titlelowerCase}.svg" alt="icon-work" class="card__image">
+            </div>
+            <div class="card__details">
+            <div class="card__activity">
+                <p class="card__title">${title}</p>
+                <img src="./images/icon-ellipsis.svg" alt="icon-ellipsis">
+            </div>
+            <div class="card__time">
+                <p class="card__hours">${element.current}hrs</p>
+                <p class="card__previous">Previous - ${element.previous} hrs</p>
+            </div>
+            </div>
+        </div>
+        `
+    })
+}
