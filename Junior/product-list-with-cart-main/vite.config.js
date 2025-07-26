d6a1d2f61800d25fe65f325eea4b8c208f52f896
lace04 <luisace04@gmail.com>
2025-07-26 11:56:06 -0500

    Característica: Integración de Tailwind CSS y eliminación de estilos no utilizados
    - Se añadió Tailwind CSS como dependencia y se configuró en vite.config.js.
    - Se eliminó App.css y se reemplazaron los estilos en index.css con la importación de Tailwind.
    - Se actualizó App.jsx para usar Tailwind para el estilo.
    - Se creó un nuevo archivo index2.html para el diseño de la lista de productos.

--- Junior/product-list-with-cart-main/vite.config.js
+++ Junior/product-list-with-cart-main/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
+  
