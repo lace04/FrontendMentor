bd1ddbad59430a2c9a61edd6abb37320104bfa1b
lace04 <luisace04@gmail.com>
2025-07-26 14:08:39 -0500

    Característica: Mejora de la estructura del componente App y adición de estilos en ProductCard

--- Junior/product-list-with-cart-main/src/App.jsx
+++ Junior/product-list-with-cart-main/src/App.jsx
@@ -2,11 +2,13 @@ import { CardsContainer } from './components/card-container';
 
 function App() {
   return (
-    <>
-      <h1>Desserts</h1>
-      <CardsContainer />
-      Your Cart Your added items will appear here
-    </>
+    <main className='flex justify-center'>
+      <section className='w-[327px]'>
+        <h1 className='text-[2.5rem] font-bold mb-[30px]'>Desserts</h1>
+        <CardsContainer />
+        Your Cart Your added items will appear here
+      </section>
+    </main>
   );
 }
 
--- Junior/product-list-with-cart-main/src/components/product-card.jsx
+++ Junior/product-list-with-cart-main/src/components/product-card.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 
-// Waffle with Berries
-//   Waffle
-//   6.50
-//   Add to Cart
-
 export const ProductCard = () => {
   return (
-    <div>
-      <img src='/public/assets/images/image-baklava-mobile.jpg' alt='Baklava' />
-      <button>
+    <div className='relative'>
+      <img
+        className='rounded-lg mb-[38px]'
+        src='/public/assets/images/image-baklava-mobile.jpg'
+        alt='Baklava'
+      />
+
+      {/* <button className='absolute inset-x-0 mx-auto top-[190px] w-40 p-3 bg-rose-50 border-[1.5px] border-rose-900 rounded-full flex justify-center items-center gap-2 cursor-pointer hover:border-red hover:text-red transition-all duration-300'>
         <img
           src='/public/assets/images/icon-add-to-cart.svg'
           alt='Add to Cart'
         />
         <span>Add to Cart</span>
-      </button>
+      </button> */}
+      <div className=' absolute inset-x-0 mx-auto top-[190px] w-40 p-3 bg-red  rounded-full flex justify-between items-center gap-2 cursor-pointer text-red-50 hover:border-red  transition-all duration-300 '>
+        <img
+          className='border border-rose-50 size-[18px] p-1 rounded-full hover:bg-rose-50 hover:text-red-900'
+          src='/public/assets/images/icon-decrement-quantity.svg'
+          alt='Decrement Quantity'
+        />
+        <p>1</p>
+        <img
+          className='border border-rose-50 size-[18px] p-1 rounded-full hover:bg-rose-50 hover:text-red-900'
+          src='/public/assets/images/icon-increment-quantity.svg'
+          alt='Increment Quantity'
+        />
+      </div>
       <p>Waffle</p>
       <h2>Waffle with Berries</h2>
       <p>$6.50</p>
     </div>
   );
 };
+
+
